ff46313ef06f9a764b566558f02558599effc84e
dani8439 <dani8439@aol.com>
2020-12-14 12:54:24 -0500

    generated Quotes index and create routs, and Quotes controller

--- db/migrate/20201208193431_create_quotes.rb
+++ db/migrate/20201208193431_create_quotes.rb
@@ -3,6 +3,7 @@ class CreateQuotes < ActiveRecord::Migration[6.0]
     create_table :quotes do |t|
       t.references :book, foreign_key: true
       t.text :quote
+      #should I change from quote to passage? lines? confusion later on.
 
       t.timestamps
     end
