7fc912ab854107bab421de70a3a933aa4f4b8bf2
dani8439 <dani8439@aol.com>
2020-12-14 14:17:02 -0500

    updated notes, cleaned up bloat in books controller index action

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,19 +3,24 @@ class Api::V1::BooksController < ApplicationController
 
   def index
     books = Book.all
-    # this is the alternative to adding macro's onto the serializer model. Seems more confusing. Ayana explains in video.
-    # options = {
-    #   #include associated quotes
-    #   include: [:quotes]
-    # # has to match association/associated models
-    # }
-    # render json: books
     render json: BookSerializer.new(books)
-    # render json: BookSerializer.new(books, options)
-    # if including options, need to pass options into render object too: render json: BookSerializer.new(books, options)
-    # don't need instance variables here,  @books = Book.all
-    # render json: book -- because rendering directly with json, instead of dealing wtih erb views.
+
   end
+
+
+    # def index
+    #   books = Book.all
+      # this is the alternative to adding macro's onto the serializer model. Seems more confusing. Ayana explains in video 2 starting at 1:20:00, and code is included in project build notes.
+      # options = {
+      #   #include associated quotes
+      #   include: [:quotes]
+      # # has to match association/associated models
+      # }
+      # render json: BookSerializer.new(books, options)
+      # if including options, need to pass options into render object too: render json: BookSerializer.new(books, options)
+      # don't need instance variables here,  @books = Book.all
+      # render json: book -- because rendering directly with json, instead of dealing wtih erb views.
+    end
 
 
   def create
