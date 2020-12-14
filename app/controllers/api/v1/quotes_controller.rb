d83e5aa1b79013248c469d3d9a52aec2d1649c3f
dani8439 <dani8439@aol.com>
2020-12-14 13:22:23 -0500

    generated QuotesController, built out create and index actions, adjusted quote serializer for book attributes I want to show

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::QuotesController < ApplicationController
+
+  def index
+    quotes = Quote.all
+    render json: QuoteSerializer.new(quotes)
+  end
+
+  def create
+    quote = Quote.new(quote_params)
+    if quote.save
+      render json: quote, status: :accepted
+    else
+      render json: {errors: quote.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:quote, :book_id)
+  end
 end
--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,4 +1,5 @@
 class QuoteSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :quote
+  attributes :quote, :book, :book_id
+  # what attributes show up in route, rendered in json. Do I need book_id AND book?
 end
