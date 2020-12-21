cf66ebf223c4c7dec074c1aece7b603687bbb095
Dani Schuhman <Dani8439@aol.com>
2020-12-21 18:14:38 -0500

    vscode

---
+++ app/controllers/api/v1/quotes_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::QuotesController < ApplicationController
+
+  def index
+    quotes = Quote.all
+    render json: QuoteSerializer.new(quotes)
+  end
+
+  def create
+    quote = Quote.new(quote_params)
+    if quote.save
+      render json: quote, status: :accepted
+    else
+      render json: {errors: quote.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:quote, :book_id)
+  end
+
+end
