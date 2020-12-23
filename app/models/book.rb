1b8fa0db3bfc63222db8ca0740abacf61626e7a7
Dani Schuhman <Dani8439@aol.com>
2020-12-23 14:15:11 -0500

    fixed typo.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   has_many :quotes, dependent: :destroy
-  accepts_nested_attributes_for :quotes, reject_if: lamda {|attributes| attributes['quote'].blank?}
+  accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}
   # to not save any quotes if quote field is blank.
 end
