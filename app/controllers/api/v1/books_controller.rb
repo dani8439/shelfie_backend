b79260fff2d01d96c23dcb37e489f55fa237ac52
Dani Schuhman <Dani8439@aol.com>
2020-12-23 13:18:31 -0500

    added .build onto book can now see quotes array, no longer blank.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,24 +6,8 @@ class Api::V1::BooksController < ApplicationController
 
   end
 
-
-    # def index
-    #   books = Book.all
-      # this is the alternative to adding macro's onto the serializer model. Seems more confusing. Ayana explains in video 2 starting at 1:20:00, and code is included in project build notes.
-      # options = {
-      #   #include associated quotes
-      #   include: [:quotes]
-      # # has to match association/associated models
-      # }
-      # render json: BookSerializer.new(books, options)
-      # if including options, need to pass options into render object too: render json: BookSerializer.new(books, options)
-      # don't need instance variables here,  @books = Book.all
-      # render json: book -- because rendering directly with json, instead of dealing wtih erb views.
-    # end
-
-
   def create
-    book = Book.new(book_params)
+    book = Book.new(book_params); book.quotes.build
     # byebug
     if book.save
       render json: book, include: :quotes, status: :accepted
@@ -42,13 +26,14 @@ class Api::V1::BooksController < ApplicationController
       render json: book, include: :quotes, status: :accepted
     else
       render json: {errors: book.errors.full_messages}, status: :unprocessable_entity
-    end 
+    end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :author, :summary, quote_ids: [])
+    # params.require(:book).permit(:title, :author, :summary, quote_ids: [])
+    params.require(:book).permit(:title, :author, :summary, quote_attributes: [:quote])
   end
 
 end
