1f0fa87d58122f346fd34194e70ed05151a5aceb
Dani Schuhman <Dani8439@aol.com>
2021-01-05 15:39:01 -0500

    error when trying to destroy.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -37,7 +37,7 @@ class Api::V1::BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :author, :summary, quotes_attributes: [:quote])
+    params.require(:book).permit(:title, :author, :summary, quotes_attributes: [:quote, :_destroy])
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
   has_many :quotes, dependent: :destroy
   accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}, allow_destroy: true
-  # # to not save any quotes if quote field is blank.
 
 end
