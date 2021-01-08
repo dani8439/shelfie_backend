acb0a1b276db880e2d2d5d94b347214c1b9a2e67
Dani Schuhman <Dani8439@aol.com>
2021-01-08 15:38:42 -0500

    note about dependent::destroy and allow_destroy.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   has_many :quotes, dependent: :destroy
-  # accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}, allow_destroy: true
-  accepts_nested_attributes_for :quotes, allow_destroy: true
+  accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}, allow_destroy: true
+  # accepts_nested_attributes_for :quotes, allow_destroy: true
+  # dependent: :destroy -- not allowing save of both records. stick with allow_destroy, permits .destroy to be called, not .delete. Why?
+
 
 end
