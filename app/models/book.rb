5a0e9a7c5f09d43f811019832a5f3ba2de9098a0
Dani Schuhman <Dani8439@aol.com>
2021-01-08 14:54:30 -0500

    not allowing delete, debugging.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   has_many :quotes, dependent: :destroy
-  accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}, allow_destroy: true
+  # accepts_nested_attributes_for :quotes, reject_if: lambda {|attributes| attributes['quote'].blank?}, allow_destroy: true
+  accepts_nested_attributes_for :quotes, allow_destroy: true
 
 end
