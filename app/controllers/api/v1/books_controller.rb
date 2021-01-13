73f1a208d87265b5179a6ac776c0ed4eb848e5b5
Dani Schuhman <Dani8439@aol.com>
2021-01-13 14:12:50 -0500

    commented out edit and update, not sure if needed yet.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -21,18 +21,18 @@ class Api::V1::BooksController < ApplicationController
     end
   end
 
-  def edit
-    book = Book.find_by(id: params[:id])
-  end
-
-  def update
-    book = Book.find_by(id: params[:id])
-    if book.update(book_params)
-      render json: book, include: :quotes, status: :accepted
-    else
-      render json: {errors: book.errors.full_messages}, status: :unprocessable_entity
-    end
-  end
+  # def edit
+  #   book = Book.find_by(id: params[:id])
+  # end
+  #
+  # def update
+  #   book = Book.find_by(id: params[:id])
+  #   if book.update(book_params)
+  #     render json: book, include: :quotes, status: :accepted
+  #   else
+  #     render json: {errors: book.errors.full_messages}, status: :unprocessable_entity
+  #   end
+  # end
 
   private
 
