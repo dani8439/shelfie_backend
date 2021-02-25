617aac17dc50800ef088dfaa1369c7a7bb4c45a6
Dani Schuhman <Dani8439@aol.com>
2021-02-25 14:23:54 -0500

    removed notes, commented out destroy.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -37,23 +37,22 @@ class Api::V1::BooksController < ApplicationController
     end
   end
 
-  def destroy
-    # raise params.inspect
-    # destroy preferred as will delete record from db and it's associated children. Delete will not.
-    book = Book.find_by(id: params[:id])
-    book.destroy
-    render json: book
-    # unless book.nil?
-    #   book.destroy
-    #   render json: book
-    # else
-    #   render json: { error: "Book not Found!" }, status: :unprocessable_entity
-    # end
-  end
+  # def destroy
+  #   # raise params.inspect
+  #   # destroy preferred as will delete record from db and it's associated children. Delete will not.
+  #   book = Book.find_by(id: params[:id])
+  #   book.destroy
+  #   render json: book
+  #   # unless book.nil?
+  #   #   book.destroy
+  #   #   render json: book
+  #   # else
+  #   #   render json: { error: "Book not Found!" }, status: :unprocessable_entity
+  #   # end
+  # end
 
   private
 
-  # possibly need [:id] passed into params so that the association can update according to stackexchange?
   def book_params
     params.require(:book).permit(:id, :title, :author, :summary, :quotes_attributes => [:id, :quote, :_destroy])
   end
