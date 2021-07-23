75c0747070cb956c863c72b0bb9e8d4542750845
Dani <dani8439@aol.com>
2021-07-23 15:20:46 -0400

    built out auth_header method, and decode_token to read the token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < ActionController::API
         JWT.encode(payload, 'my_s3cr3t')
     end 
 
+    def auth_header 
+        # { Authorization: 'Bearer <token>' }
+        request.headers['Authorization']
+    end 
+
+    # WHY?: `JWT.decode` takes three arguments as well: a JWT as a string, an application secret, and––optionally––a hashing algorithm.
+    def decode_token 
+        if auth_header
+            token = auth_header.split(' ')[1]
+            # get token into a readable snippet, split at the space.
+            begin 
+                JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+            rescue JWT::DecodeError 
+                nil 
+            end 
+        end 
+    end 
 end
