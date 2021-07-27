743db0416d549f0d106cbe165b5c69b00f8bfb0f
Dani <dani8439@aol.com>
2021-07-27 14:34:59 -0400

    added post to routes, and checked with postman that everything is working.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create 
-        @user - User.find_by(email: user_login_params[:email])
+        @user = User.find_by(email: user_login_params[:email])
         # if user is found, then given the opportunity is given to authenticate them. .authenticate comes from BCRYPT
         if @user && @user.authenticate(user_login_params[:password])
             token = encode_token({ user_id: @user.id })
@@ -11,6 +11,8 @@ class Api::V1::AuthController < ApplicationController
             render json: { messsage: 'Invalid email or password' }, status: :unauthorized
         end 
     end
+
+    private
 
     def user_login_params
         params.require(:user).permit(:email, :password)
