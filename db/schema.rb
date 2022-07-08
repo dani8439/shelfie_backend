c7a6a3e707a3448d3cb51d026fb0e8202d43e63f
Dani Schuhman <dani8439@aol.com>
2022-07-08 12:27:07 -0400

    toying. goddamn heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_173757) do
+ActiveRecord::Schema.define(version: 2020_12_08_193431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2021_07_12_173757) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
   add_foreign_key "books", "users"
